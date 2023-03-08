9f91d5e0b936ae2d55c332c44d3d552c8e2c6c98
Micaela Rosadio <rosadio.karen@gmail.com>
2023-03-08 17:54:27 +0100

    Generate map with polygons of all animal habitats

---
+++ components/AllHabitatsMap.jsx
@@ -0,0 +1,25 @@
+import { useTranslation } from 'next-i18next';
+import mapboxgl from 'mapbox-gl';
+import { all } from 'axios';
+import data from './../public/locales/en/animals.json';
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_ACCESS_TOKEN;
+
+function AllHabitatsMap() {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v12',
+    center: [-121.403732, 40.492392],
+    zoom: 10
+  });
+  
+  // Add polygons
+  map.addSource('data', data[0][1].habitats[0].polygon)
+  return (
+    <div className='container'>
+      <div id='map' />
+    </div>
+  )
+};
+
+export default AllHabitatsMap;
