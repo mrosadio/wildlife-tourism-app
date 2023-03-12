27fe414971e55582c6f6a10c44f451e91341d686
Micaela Rosadio <rosadio.karen@gmail.com>
2023-03-12 22:17:25 +0100

    Add layer to polygons

--- src/components/AllHabitatsMap.jsx
+++ src/components/AllHabitatsMap.jsx
@@ -7,15 +7,14 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_ACCESS_TOKEN;
 
 // Retrieve polygons from dataset
 let habitatPolygons = [];
-for (let animal of (data)) {
+for (let animal of data) {
   for (let habitat of animal.habitats) {
     //console.log(habitat.polygon)
-    habitatPolygons.push(habitat.polygon)
+    habitatPolygons.push(habitat.polygon);
   }
 }
 
 function AllHabitatsMap() {
-
   useEffect(() => {
     function initializeMap() {
       const mapAllAnimals = new mapboxgl.Map({
@@ -23,19 +22,31 @@ function AllHabitatsMap() {
         style: "mapbox://styles/mapbox/outdoors-v12",
         center: [-77.042793, -12.046374],
         zoom: 3,
-      })
-      mapAllAnimals.on("load", () => {
-        mapAllAnimals.addSource('habitats', {
-          'type': 'geojson',
-          'data': {
-            'type': 'Feature',
-            'geometry': {
-              'type': 'Polygon',
-              'coordinates': habitatPolygons
-            }
-          }
-        })
       });
+      mapAllAnimals.on("load", () => {
+        mapAllAnimals.addSource("habitats", {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            geometry: {
+              type: "Polygon",
+              coordinates: [habitatPolygons],
+            },
+          },
+        });
+      });
+      // Add a new layer to visualize the polygon.
+      mapAllAnimals.addLayer({
+        id: "habitats",
+        type: "fill",
+        source: "habitats",
+        layout: {},
+        paint: {
+          "fill-color": "#0080ff", // blue color fill
+          "fill-opacity": 0.5
+        },
+      });
+
     }
     initializeMap();
   }, []);
@@ -45,7 +56,6 @@ function AllHabitatsMap() {
       <div id="mapAll" />
     </div>
   );
-
 }
 
 export default AllHabitatsMap;
